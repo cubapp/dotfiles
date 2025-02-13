c154051c658b1c25e796b2e2ef9a0d9dabfd7fa4
Jakub Urbanec <cubapp@users.noreply.github.com>
2025-02-13 08:35:20 +0100

    Create setup.sh
    Initial commit

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Script for setting up aliases and tmux config. 
+echo "Setting up aliases and tmux.config. Please review both files as well as this script. Trust no one :-)"
+echo "-------------------------------------------------------------------------------------------------"
+
+if ! [ -f ~/.aliases ]; then
+  echo "Copying .aliases to HOME dir"
+  cp aliases ~/.aliases || echo "Error: Aliases files not copied"
+fi
+
+
+if ! [ -f ~/.tmux.conf ]; then
+  echo "Copying .tmux.conf to HOME dir"
+  cp .tmux.conf ~/.tmux.conf || echo "Error: tmux config not copied"
+fi
+
+
+echo "Setting up aliases in .bashrc or .zshrc"
+
+if  [ -f ~/.bashrc ] ; then
+  echo "There is .bashrc in your HOME dir. Adding the .aliases to sources"
+  echo "[ -f ~/.aliases ] && source ~/.aliases "  >> ~/.bashrc || echo "Error: unable to add .aliases into .bashrc"
+else
+  echo "There is no .bashrc in your HOME dir. Hmms. Adding the .aliases to sources in .zshrc"
+  echo "[ -f ~/.aliases ] && source ~/.aliases "  >> ~/.zshrc  || echo "Error: unable to add .aliases into .zshrc"
+fi
